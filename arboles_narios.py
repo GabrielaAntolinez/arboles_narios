b69a53dee9ed5709748a8ebf7886e0544cb1f431
Juan Camilo Espitia <30874068+20152020040@users.noreply.github.com>
2018-04-09 16:38:48 -0500

    Add files via upload

---
+++ arboles_narios.py
@@ -0,0 +1,21 @@
+class Nodo_n:
+    def __init__ (self, valor, hijos=[]):
+        self.valor=valor
+        self.hijos=hijos
+
+def buscar(arbol,valor):
+    if arbol==None:
+        return False
+    if arbol.valor==valor:
+        return True
+    return buscar_hijos(arbol.hijos,valor)
+
+def buscar_hijos(lista,valor):
+    if lista==[]:
+        return False
+    return buscar(lista[0],valor) or buscar_hijos(lista[1:],valor)
+
+
+
+a=Nodo_n(2500,[Nodo_n(50000),Nodo_n(250000),Nodo_n(2500000)])
+buscar(a,2500)
